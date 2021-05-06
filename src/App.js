70e0b9f3e7e02b5e60da2bce7ef6639b01804d73
kpodsiadlo <kpodsiadlo@gmail.com>
2021-05-06 21:33:57 +0200

    props sent to the function require {}, dammit...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import User from './Components/User'
 import Reservations from "./Components/Reservations"
-
 import './App.css';
+import { useState } from 'react'
+import AddReservation from "./Components/AddReservation"
+
 
 const App = () => {
 
+  const [reservations, setReservations] = useState([
+    {
+      id: 1,
+      roomNumber: 234,
+      startTime: "2023-08-19T23:15:30.000Z",
+      endTime: "2045-08-19T23:19:30.000Z",
+    },
+    {
+      id: 2,
+      roomNumber: 13,
+      startTime: "1975-08-19T23:15:30.000Z",
+      endTime: "1975-08-19T23:15:30.000Z",
+    },
+    {
+      id: 3,
+      roomNumber: 18,
+      startTime: "1975-08-19T23:15:30.000Z",
+      endTime: "1975-08-19T23:15:30.000Z",
+    },
+  ])
+
+  const [showAddReservation, setShowAddReservation] = useState(
+    false
+  )
+
+  const Remove = (id) => {
+    setReservations(reservations.filter(reservation => reservation.id !== id))
+  }
+
+  const toggleAddReservation = () => {
+    setShowAddReservation(!showAddReservation)
+  }
+
   return (
     <div className="App">
-      <User/>
-      <Reservations/>
+      <User onToggleAddReservation={toggleAddReservation} />
+      {showAddReservation && <AddReservation/>}
+      <Reservations reservations={reservations} onRemove={Remove} />
     </div>
   );
 }
